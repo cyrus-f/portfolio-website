e76fe9dc0ef1b7ed8cab37f5e159c80b120d85e9
cyrus-f <67000631+cyrus-f@users.noreply.github.com>
2024-06-19 12:13:14 +1000

    change javascript to switch between style sheets
    fixed darkmode buttons
    still working on boxes

--- script.js
+++ script.js
@@ -8,54 +8,10 @@ function toggleMenu() {
 }
 
 function toggleDarkMode() {
-    var links = document.getElementsByTagName("a");
-    var buttons = document.getElementsByTagName("button");
-    var icons = document.getElementsByClassName("icon")
-    if (!isDarkMode) {
-        /* toggle variable*/
-        isDarkMode = true;
-
-        /* make background black and text white (some things are not included) */
-        document.body.style.backgroundColor = "black";
-        document.body.style.color = "white";
-        
-        /* fix links */
-        for(var i=0;i<links.length;i++) {
-            links[i].style.color = "white";
-        }
-
-        /* fix buttons */
-        for(var i=0;i<buttons.length;i++) {
-            buttons[i].style.color = "white";
-        }
-
-        /** fix github and linkedin icons */
-        for(var i=0;i<icons.length;i++) {
-            icons[i].style.filter = "invert(100%)";
-        }
-    } else {
-        /* toggle variable*/
-        isDarkMode = false;
-
-        /* make background white and text black (some things are not included) */
-        document.body.style.backgroundColor = "white";
-        document.body.style.color = "black";
-        
-        /* fix links */
-        for(var i=0;i<links.length;i++) {
-            links[i].style.color = "black";
-        }
-
-        /* fix buttons */
-        for(var i=0;i<buttons.length;i++) {
-            console.log(buttons[i].classList)
-            /* buttons[i].style.color = "black"; */
-        }
-
-        /** fix github and linkedin icons */
-        for(var i=0;i<icons.length;i++) {
-            icons[i].style.filter = "none";
-        }
-        
-    }
+    var theme = document.getElementsByTagName('link')[1];
+    if (theme.getAttribute('href') == 'light.css') { 
+        theme.setAttribute('href', 'dark.css'); 
+    } else { 
+        theme.setAttribute('href', 'light.css'); 
+    } 
 }
